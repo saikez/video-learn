45b58f2032296a95917f662a167ef71c01741f1e
Simon Waldron <saikez@yahoo.com>
2022-10-10 06:08:11 +0100

    Add feature and model specs

--- spec/features/user_watches_a_lesson_spec.rb
+++ spec/features/user_watches_a_lesson_spec.rb
@@ -4,6 +4,7 @@ RSpec.feature "User watches a lesson", type: :feature do
   let(:user) {create(:user)}
   let(:course) {create(:course)}
   let!(:lesson){create(:lesson, course: course)}
+  let!(:comment){create(:comment, lesson: lesson, user: user)}
 
   it 'allows the user to watch a lesson' do
     sign_in user
@@ -13,5 +14,17 @@ RSpec.feature "User watches a lesson", type: :feature do
     expect(page).to have_content(course.description)
     click_link lesson.title
     expect(find('iframe')[:src]).to eq(lesson.video_link)
+  end
+
+  it 'allows the user to comment on a lesson' do
+    sign_in user
+    visit root_path
+    click_link course.title
+    click_link lesson.title
+
+    expect(page).to have_content(comment.body)
+    fill_in('comment-form-body', with: 'The best comment in the world')
+    click_button 'Create Comment'
+    expect(find('#comments')).to have_content('The best comment in the world')
   end
 end
